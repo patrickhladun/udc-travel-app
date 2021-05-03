f31536aeba58c9b05e457e5e2f0eb02a729eb04f
Patrick <patrickhladun@gmail.com>
2021-05-03 21:18:39 +0100

    Move getForecastWeather to own file

--- src/client/js/showDestinations.js
+++ src/client/js/showDestinations.js
@@ -1,27 +1,5 @@
-import moment from 'moment';
+import { getForecastWeather } from './getForecastWeather';
 import { arrow, remove } from './icons';
-
-const getForecastWeather = (forecast) => {
-    const fragment = document.createDocumentFragment();1
-
-    const columns = forecast.map(day => {
-        const column = document.createElement('div');
-        const weekDay = moment(day.datetime).format('ddd');
-        const date = moment(day.datetime).format('DD');
-        const month = moment(day.datetime).format('MMM');
-        column.classList.add('forecast__column');
-        const columnHTML = `
-            <div class="forecast__month">${month}</div>
-            <div class="forecast__day">${weekDay}</div>
-            <div class="forecast__date">${date}</div>
-            <img class="forecast__icon" src="/images/icons/${day.weather.icon}.png" />
-            <div class="forecast__temp">${day.temp}&nbsp;℃</div>
-        `;
-        column.innerHTML = columnHTML;
-        fragment.appendChild(column);
-    });
-    return fragment;
-};
 
 export const showDestinations = (tripId) => {
     const destinations = JSON.parse(localStorage.getItem('destinations'));
