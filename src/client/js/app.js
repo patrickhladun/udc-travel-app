e9693b8247e7cb998fb75144d39c9b7e27f5e028
Patrick <patrickhladun@gmail.com>
2021-05-03 14:30:36 +0100

    Style forecast weather

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -139,11 +139,15 @@ const getForecastWeather = (forecast) => {
     const columns = forecast.map(day => {
         const column = document.createElement('div');
         const weekDay = moment(day.datetime).format('ddd');
+        const date = moment(day.datetime).format('DD');
+        const month = moment(day.datetime).format('MMM');
         column.classList.add('forecast__column');
         const columnHTML = `
+            <div class="forecast__month">${month}</div>
             <div class="forecast__day">${weekDay}</div>
+            <div class="forecast__date">${date}</div>
             <img class="forecast__icon" src="/images/icons/${day.weather.icon}.png" />
-            <div class="forecast__temp">${day.temp} ℃</div>
+            <div class="forecast__temp">${day.temp}&nbsp;℃</div>
         `;
         column.innerHTML = columnHTML;
         fragment.appendChild(column);
