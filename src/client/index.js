84ef165ba36f4e6a5fa72b650323974d961a1f47
Patrick <patrickhladun@gmail.com>
2021-05-04 09:44:41 +0100

    Update styles

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,17 @@ import { showTrips } from './js/showTrips';
 import { getBackground } from './js/getBackground';
 import './styles/style.scss';
 
+// TODO: add mobile styles
+// TODO: add hover stated
+// TODO: add validation to add a trip form and display errors
+// TODO: pull countries and cities
+// TODO: fix issues with double trips adding up
+// TODO: filter trips
+// TODO: update date fields to not be able to select previous dates
+// TODO: add service workers
+// TODO: fix styles naming
+// TODO: add images where images is not available
+
 getBackground();
 tripForm();
 showTrips();
